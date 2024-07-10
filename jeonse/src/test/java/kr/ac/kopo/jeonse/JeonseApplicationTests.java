b10848bcc17b573886051daa38d4c7b2dc591720
mj.kim <alswo9853@gmail.com>
2024-07-10 16:31:50 +0900

    Initial commit

---
+++ jeonse/src/test/java/kr/ac/kopo/jeonse/JeonseApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.jeonse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeonseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
