0b67ea683386a52807c9e660255eaac38e3e32fd
mj.kim <alswo9853@gmail.com>
2024-07-11 19:32:15 +0900

    Feat: Get real estate list

--- src/main/java/kr/ac/kopo/jeonse/domain/jeonse/mapper/JeonseMapper.java
+++ src/main/java/kr/ac/kopo/jeonse/domain/jeonse/mapper/JeonseMapper.java
@@ -2,12 +2,17 @@ package kr.ac.kopo.jeonse.domain.jeonse.mapper;
 
 import kr.ac.kopo.jeonse.domain.jeonse.domain.Jeonse;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface JeonseMapper {
 
     @Select("SELECT * FROM jeonse WHERE atclNo = #{atclNo}")
     Jeonse selectJeonseByAtclNo(String atclNo);
+
+    List<Jeonse> selectRemainJeonse(@Param("address") String address,@Param("aptName") String aptName);
 }
 
