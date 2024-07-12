fa036ff7c7d202a2ce100189e80336d2f17291a7
mj.kim <alswo9853@gmail.com>
2024-07-12 21:13:39 +0900

    Feat: Github action - Add auto service change and restart

---
+++ server-script/after_deploy.sh
@@ -0,0 +1,4 @@
+#!bin/bash
+
+mv /home/vincentkim0503/jeonse-0.0.1-SNAPSHOT.jar /home/vincentkim0503/backend/jeonse.jar
+sudo systemctl start yebang.service
---
+++ server-script/before_deploy.sh
@@ -0,0 +1,4 @@
+#!bin/bash
+
+sudo systemctl stop yebang.service
+mv /home/vincentkim0503/backend/jeonse.jar /home/vincentkim0503/backend/archieve/jeonse.$(date +%Y-%m-%d_%H:%M).jar
